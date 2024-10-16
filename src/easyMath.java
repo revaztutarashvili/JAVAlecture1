f1a8a9c4390847b0c77f0048ea3a22c463a7c569
Revaz Tutarashvili <140757300+revaztutarashvili@users.noreply.github.com>
2024-10-17 02:14:47 +0400

    create first homework

---
+++ src/easyMath.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+// this code calculate: X+Y-Z
+public class easyMath {
+    public static void main(String[] args) {
+        Scanner scannerinput = new Scanner(System.in);
+        System.out.print("please input X: ");
+        int inputX = scannerinput.nextInt();
+        System.out.print("please input Y: ");
+        int inputY = scannerinput.nextInt();
+        System.out.print("please input Z: ");
+        int inputZ = scannerinput.nextInt();
+        int result = inputX + inputY - inputZ;
+        System.out.println("Result is: " + result);
+
+    }
+}
