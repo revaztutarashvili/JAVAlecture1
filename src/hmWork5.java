6a7a3a2253b74f2d4827d88c465f0cfadbe522ce
Revaz Tutarashvili <140757300+revaztutarashvili@users.noreply.github.com>
2024-10-21 23:49:44 +0400

    done hmwrk 5

--- src/hmWork5.java
+++ src/hmWork5.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class hmWork5 {
     public static void main (String[] args) {
         for(int i=1; i<=6; i++) {
-            for(int j=1; j<=6-i; j++) {
+            for(int j=0; j<=6-i; j++) {
                 System.out.print(" ");
             }
             for(int k=1; k<=i; k++) {
